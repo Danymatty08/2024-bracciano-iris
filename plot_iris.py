ddd7a4b26deb5011a37e2ef2d40f1be243ef4864
Your Name <you@example.com>
2024-11-28 16:09:51 +0100

    add petal plot_iris

--- plot_iris.py
+++ plot_iris.py
@@ -22,6 +22,16 @@ plt.title('Distribuzione dei Sepali')
 plt.legend()
 
 # Plot per i petali
+plt.subplot(1, 2, 2)
+
+for i in range(3):
+    mask = y == i
+
+    plt.scatter(X[mask, 2], X[mask, 3], label=iris.target_names[i])
+plt.xlabel('Lunghezza petalo')
+plt.ylabel('Larghezza petalo')
+plt.title('Distribuzione dei petali')
+plt.legend()
 
 # Ora tocca a te! Prova a fare il plot dei petali!
 # Poi fai commit e push
